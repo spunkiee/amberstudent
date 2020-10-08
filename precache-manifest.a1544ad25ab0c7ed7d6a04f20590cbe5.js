a351e67a2d6b424c77c69f01f90a6b19cc2f1256
spunkiee <ramashishkhush@gmail.com>
2020-10-08 18:05:04 +0530

    Updates

---
+++ precache-manifest.a1544ad25ab0c7ed7d6a04f20590cbe5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e188082760cae346d69f5439093d118",
+    "url": "/amberstudent/index.html"
+  },
+  {
+    "revision": "60561d206a6da8a1d770",
+    "url": "/amberstudent/static/css/main.0cc65d4f.chunk.css"
+  },
+  {
+    "revision": "0da91c573f98caca968e",
+    "url": "/amberstudent/static/js/2.42dd3555.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amberstudent/static/js/2.42dd3555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60561d206a6da8a1d770",
+    "url": "/amberstudent/static/js/main.214bc6db.chunk.js"
+  },
+  {
+    "revision": "bf1c21a2ef67aeb7c163",
+    "url": "/amberstudent/static/js/runtime-main.5174e2bc.js"
+  },
+  {
+    "revision": "da25b2a33bba68386e2846b44c7896d2",
+    "url": "/amberstudent/static/media/blonde-1867768.da25b2a3.jpg"
+  },
+  {
+    "revision": "4ab92a923b10587ac9a2bcb082dfe082",
+    "url": "/amberstudent/static/media/location.4ab92a92.png"
+  }
+]);
